e365630ee2cc2bf9780f49925962da43945035cb
Thanuchiya Thanancheyan <thanuchiya@gmail.com>
2019-04-16 16:32:50 +0200

    controle

---
+++ src/main/java/fr/thanuchiya/l3pcontrolesitemap/L3pControleSitemapApplication.java
@@ -0,0 +1,13 @@
+package fr.thanuchiya.l3pcontrolesitemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L3pControleSitemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L3pControleSitemapApplication.class, args);
+	}
+
+}

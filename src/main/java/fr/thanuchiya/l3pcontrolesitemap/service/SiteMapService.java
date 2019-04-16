2d8b668036941fc4d95d46ceb59841a0e8bcd725
Thanuchiya Thanancheyan <thanuchiya@gmail.com>
2019-04-16 17:08:07 +0200

    controle

--- src/main/java/fr/thanuchiya/l3pcontrolesitemap/service/SiteMapService.java
+++ src/main/java/fr/thanuchiya/l3pcontrolesitemap/service/SiteMapService.java
@@ -1,11 +1,9 @@
 package fr.thanuchiya.l3pcontrolesitemap.service;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "siteMapService")
-@XmlAccessorType(XmlAccessType.FIELD)
+import org.springframework.stereotype.Component;
+
+@Component
 public class SiteMapService {
 
 

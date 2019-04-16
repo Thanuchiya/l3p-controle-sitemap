12505e258658d6c499c48a2c28b69ef0340b36b8
Thanuchiya Thanancheyan <thanuchiya@gmail.com>
2019-04-16 17:01:49 +0200

    controle

--- src/main/java/fr/thanuchiya/l3pcontrolesitemap/service/SiteMapService.java
+++ src/main/java/fr/thanuchiya/l3pcontrolesitemap/service/SiteMapService.java
@@ -1,4 +1,13 @@
 package fr.thanuchiya.l3pcontrolesitemap.service;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "siteMapService")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class SiteMapService {
+
+
+
 }
